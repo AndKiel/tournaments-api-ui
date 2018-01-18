7919687a84cf369ba2acb92d524edfd8c7d73c64
Andrzej Kieltyka <a.kieltyka@gmail.com>
2018-01-18 08:45:05 +0100

    Define all api routes

---
+++ src/utils/api_routes.js
@@ -0,0 +1,26 @@
+const apiRoutes = {
+  signUpUsers: () => "/users/sign_up",
+
+  oauthToken: () => "/oauth/token",
+  oauthTokenInfo: () => "/oauth/token/info",
+  oauthRevoke: () => "/oauth/revoke",
+
+  user: () => "/user",
+
+  tournaments: () => "/tournaments",
+  enlistedTournaments: () => "/tournaments/enlisted",
+  tournament: id => `/tournaments/${id}`,
+  startTournament: id => `/tournaments/${id}/start`,
+  endTournament: id => `/tournaments/${id}/end`,
+
+  competitor: () => "/competitor",
+  confirmCompetitor: id => `/competitors/${id}/confirm`,
+
+  rounds: () => "/rounds",
+  round: id => `/rounds/${id}`,
+
+  players: () => "/players",
+  player: id => `/players/${id}`
+};
+
+export default apiRoutes
