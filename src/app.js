5cad0b8358341d093fb218338cc9fad891e8d966
Andrzej Kieltyka <a.kieltyka@gmail.com>
2018-01-19 07:37:05 +0100

    Implement basic mechanisms for api client

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'mobx-react';
 
 import Navigation from './components/navigation';
 import Home from './pages/home';
+import ApiClient from './utils/api_client';
+import AppStore from './stores/app_store';
+
+const apiClient = new ApiClient();
+const appStore = AppStore.create({}, { apiClient });
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navigation />
-        <BrowserRouter>
-          <Route exact path="/" component={Home} />
-        </BrowserRouter>
-      </div>
+      <Provider store={appStore}>
+        <div>
+          <Navigation />
+          <BrowserRouter>
+            <Route exact path="/" component={Home} />
+          </BrowserRouter>
+        </div>
+      </Provider>
     );
   }
 }
--- src/utils/api_client.js
+++ src/utils/api_client.js
@@ -1,3 +1,99 @@
-class ApiClient {}
+import assign from 'lodash/assign';
+import axios from 'axios';
+import apiRoutes from './api_routes';
+import AccessToken from '../models/access_token';
+
+class ApiClient {
+  constructor() {
+    this.apiUrl = process.env.REACT_APP_API_URL;
+    this.token = null;
+  }
+
+  // Requests
+
+  async get(url, opts) {
+    return await this.send(assign({ url: url, method: 'GET' }, opts));
+  }
+  async post(url, opts) {
+    return await this.send(assign({ url: url, method: 'POST' }, opts));
+  }
+  async patch(url, opts) {
+    return await this.send(assign({ url: url, method: 'PATCH' }, opts));
+  }
+  async delete(url, opts) {
+    return await this.send(assign({ url: url, method: 'DELETE' }, opts));
+  }
+
+  async send(opts) {
+    const request = assign({ baseUrl: this.apiURL, authenticate: false }, opts);
+
+    if (request.authenticate === true && this.token !== null) {
+      request.headers = { Authorization: `Bearer ${this.token.access_token}` };
+    }
+
+    try {
+      const response = await axios(request);
+      return response.data;
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status === 401 &&
+        this.token !== null
+      ) {
+        try {
+          await this.refreshToken();
+          return this.send(request);
+        } catch (error) {
+          throw error;
+        }
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  // Authentication
+
+  async requestToken(email, password) {
+    try {
+      const response = await this.post(apiRoutes.oauthToken(), {
+        data: { email, password, grant_type: 'password' }
+      });
+      this.setToken(response.data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async refreshToken() {
+    try {
+      const response = await this.post(apiRoutes.oauthToken(), {
+        data: {
+          refresh_token: this.token.refresh_token,
+          grant_type: 'refresh_token'
+        }
+      });
+      this.setToken(response.data);
+    } catch (error) {
+      this.nullifyToken();
+      throw error;
+    }
+  }
+
+  async revokeToken() {
+    await this.post(apiRoutes.oauthRevoke());
+    this.nullifyToken();
+  }
+
+  // Helpers
+
+  setToken(token) {
+    this.token = AccessToken.create(token);
+  }
+
+  nullifyToken() {
+    this.token = null;
+  }
+}
 
 export default ApiClient;
