2cf7a42877eb99bfdde1c5ce2b3132fd5e0c0130
Andrzej Kieltyka <a.kieltyka@gmail.com>
2018-01-22 13:44:25 +0100

    Style alerts

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { Paper, Typography } from 'material-ui';
+import styles from './alert.scss';
 
 const Alert = ({ store: { uiStore: { alertText, alertType } } }) => {
   if (alertText.length > 0) {
     return (
-      <Paper classes={`alert alert-${alertType}`}>
+      <Paper className={styles[`alert-${alertType}`]}>
         <Typography>{alertText}</Typography>
       </Paper>
     );
