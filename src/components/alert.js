615990e87b8aae9e6831811bf28084bea2c25653
Andrzej Kieltyka <a.kieltyka@gmail.com>
2018-01-26 11:16:33 +0100

    Refactor alert to use snackbar

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-import { Paper, Typography } from 'material-ui';
+import { Fade, Snackbar, SnackbarContent } from 'material-ui';
 import styles from './alert.scss';
 
-const Alert = ({ store: { uiStore: { alertText, alertType } } }) => {
-  if (alertText.length > 0) {
-    return (
-      <Paper className={styles[`alert-${alertType}`]}>
-        <Typography>{alertText}</Typography>
-      </Paper>
-    );
-  } else return null;
+const Alert = ({
+  store: { uiStore: { alertText, alertType, isAlertOpen } }
+}) => {
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left'
+      }}
+      transition={Fade}
+      open={isAlertOpen}
+    >
+      <SnackbarContent
+        className={styles[`alert-${alertType}`]}
+        message={alertText}
+      />
+    </Snackbar>
+  );
 };
 
 export default inject('store')(observer(Alert));
--- src/stores/ui_store.js
+++ src/stores/ui_store.js
@@ -3,18 +3,19 @@ import { types } from 'mobx-state-tree';
 const UIStore = types
   .model('UIStore', {
     alertText: types.optional(types.string, ''),
-    alertType: types.optional(types.string, '')
+    alertType: types.optional(types.string, ''),
+    isAlertOpen: types.optional(types.boolean, false)
   })
   .actions(self => {
     return {
       closeAlert() {
-        self.alertType = '';
-        self.alertText = '';
+        self.isAlertOpen = false;
       },
 
       setAlert(type, text) {
         self.alertType = type;
         self.alertText = text;
+        self.isAlertOpen = true;
         setTimeout(() => self.closeAlert(), 5000);
       }
     };
