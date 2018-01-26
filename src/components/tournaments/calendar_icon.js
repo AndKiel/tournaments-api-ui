9c363a1a407f795b9abef7226cb228567551736d
Andrzej Kieltyka <a.kieltyka@gmail.com>
2018-01-26 14:42:25 +0100

    Style tournament card

---
+++ src/components/tournaments/calendar_icon.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react/index';
+import { Typography } from 'material-ui';
+import moment from 'moment';
+import styles from './calendar_icon.scss';
+
+@observer
+class CalendarIcon extends Component {
+  render() {
+    const date = moment(this.props.date);
+
+    return (
+      <div className={styles['calendar-icon']}>
+        <Typography component="span" className={styles['calendar-icon-month']}>
+          {date.format('MMM')}
+        </Typography>
+        <Typography component="span" className={styles['calendar-icon-day']}>
+          {date.format('D')}
+          <Typography component="span" className={styles['calendar-icon-dow']}>
+            {date.format('ddd')}
+          </Typography>
+        </Typography>
+      </div>
+    );
+  }
+}
+
+export default CalendarIcon;
--- src/components/tournaments/tournament_card.js
+++ src/components/tournaments/tournament_card.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react/index';
 import { Card, CardContent, Typography } from 'material-ui';
+import CalendarIcon from './calendar_icon';
+import moment from 'moment';
+import styles from './tournament_card.scss';
 
 @observer
 class TournamentCard extends Component {
@@ -13,10 +16,13 @@ class TournamentCard extends Component {
     } = this.props.tournament;
 
     return (
-      <Card>
+      <Card className={styles['tournament-card']}>
+        <CardContent>
+          <CalendarIcon date={starts_at} />
+        </CardContent>
         <CardContent>
           <Typography type="title">{name}</Typography>
-          <Typography>Starts at: {starts_at}</Typography>
+          <Typography>Starts at {moment(starts_at).format('HH:mm')}</Typography>
           <Typography>Competitors limit: {competitors_limit}</Typography>
           <Typography>Status: {status}</Typography>
         </CardContent>
