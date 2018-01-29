0b4011a31d7f45e4643782c39cdedfa164d7cfaf
Andrzej Kieltyka <a.kieltyka@gmail.com>
2018-01-29 12:58:48 +0100

    Refactor tournament form and new tournament page

--- src/forms/tournament_form.js
+++ src/forms/tournament_form.js
@@ -1,79 +1,13 @@
-import React, { Component } from 'react';
-import { observable } from 'mobx';
-import { inject, observer } from 'mobx-react';
-import autobind from 'autobind-decorator';
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  TextField,
-  Typography
-} from 'material-ui';
+import { Form } from 'mobx-react-form';
 
-@inject('store')
-@observer
-class TournamentForm extends Component {
-  @observable
-  formData = {
-    tournament: {
-      competitors_limit: '',
-      description: '',
-      name: '',
-      result_names: [],
-      starts_at: ''
-    }
-  };
-  @observable errors = {};
-
-  @autobind
-  async submitForm(e) {
-    try {
-      e.preventDefault();
-      this.errors = {};
-      await this.props.store.organisedTournamentsStore.createTournament(
-        this.formData
-      );
-      this.props.store.uiStore.setAlert(
-        'success',
-        'You have successfully created a tournament.'
-      );
-    } catch (error) {
-      if (error.response && error.response.data && error.response.data.fields) {
-        this.errors = error.response.data.fields;
-      } else {
-        throw error;
+class TournamentForm extends Form {
+  setup() {
+    return {
+      fields: ['tournament.name'],
+      labels: {
+        'tournament.name': 'Name'
       }
-    }
-  }
-
-  render() {
-    return (
-      <Card>
-        <form onSubmit={this.submitForm}>
-          <CardContent>
-            <Typography type="headline">New tournament</Typography>
-            <TextField
-              id="name"
-              label="Name"
-              value={this.formData.tournament.name}
-              error={this.errors.name}
-              helperText={this.errors.name ? this.errors.name.join(', ') : ''}
-              onChange={e => (this.formData.tournament.name = e.target.value)}
-              margin="normal"
-              autoFocus
-              required
-              fullWidth
-            />
-          </CardContent>
-          <CardActions>
-            <Button color="primary" style={{ flex: 1 }} type="submit">
-              Create
-            </Button>
-          </CardActions>
-        </form>
-      </Card>
-    );
+    };
   }
 }
 
--- src/pages/tournaments/new_tournament_page.js
+++ src/pages/tournaments/new_tournament_page.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
-import { Grid } from 'material-ui';
+import { inject } from 'mobx-react/index';
+import autobind from 'autobind-decorator';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography
+} from 'material-ui';
 import TournamentForm from '../../forms/tournament_form';
+import TextInput from '../../components/forms/text_input';
 
+@inject('store')
 class NewTournamentPage extends Component {
+  componentWillMount() {
+    this.form = new TournamentForm();
+  }
+
+  @autobind
+  async submitForm(e) {
+    try {
+      e.preventDefault();
+      await this.form.validate();
+      await this.props.store.organisedTournamentsStore.createTournament(
+        this.form.values()
+      );
+      this.props.store.uiStore.setAlert(
+        'success',
+        'You have successfully created a tournament.'
+      );
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.fields) {
+        for (let pair of Object.entries(error.response.data.fields)) {
+          this.form.$(`tournament.${pair[0]}`).invalidate(pair[1].join(', '));
+        }
+      } else {
+        throw error;
+      }
+    }
+  }
+
   render() {
     return (
       <Grid container justify="center">
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <TournamentForm />
+          <Card>
+            <form onSubmit={this.submitForm}>
+              <CardContent>
+                <Typography type="headline">New tournament</Typography>
+                <TextInput
+                  field={this.form.$('tournament.name')}
+                  autoFocus
+                  required
+                />
+              </CardContent>
+              <CardActions>
+                <Button color="primary" style={{ flex: 1 }} type="submit">
+                  Create
+                </Button>
+              </CardActions>
+            </form>
+          </Card>
         </Grid>
       </Grid>
     );
