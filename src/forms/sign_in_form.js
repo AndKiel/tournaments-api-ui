2715d9bcc04226cb22475ccea1d08c658eec2771
Andrzej Kieltyka <a.kieltyka@gmail.com>
2018-01-29 12:22:51 +0100

    Refactor sign in form and page

--- src/forms/sign_in_form.js
+++ src/forms/sign_in_form.js
@@ -1,91 +1,19 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
-import { observable } from 'mobx';
-import { inject, observer } from 'mobx-react';
-import autobind from 'autobind-decorator';
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  TextField,
-  Typography
-} from 'material-ui';
-import routes from '../utils/routes';
+import { Form } from 'mobx-react-form';
 
-@withRouter
-@inject('store')
-@observer
-class SignInForm extends Component {
-  @observable
-  formData = {
-    email: '',
-    password: ''
-  };
-  @observable error = null;
-
-  @autobind
-  async submitForm(e) {
-    try {
-      e.preventDefault();
-      this.error = null;
-      await this.props.store.sessionStore.signIn(this.formData);
-      this.props.history.push(routes.tournaments());
-      this.props.store.uiStore.setAlert(
-        'success',
-        'You have successfully signed in.'
-      );
-    } catch (error) {
-      if (
-        error.response &&
-        error.response.data &&
-        error.response.data.error_description
-      ) {
-        this.error = error.response.data.error_description;
-      } else {
-        throw error;
-      }
-    }
-  }
-
-  render() {
-    return (
-      <Card>
-        <form onSubmit={this.submitForm}>
-          <CardContent>
-            <Typography type="headline">Sign in</Typography>
-            <TextField
-              id="email"
-              label="Email"
-              value={this.formData.email}
-              error={!!this.error}
-              onChange={e => (this.formData.email = e.target.value)}
-              margin="normal"
-              autoFocus
-              required
-              fullWidth
-            />
-            <TextField
-              id="password"
-              label="Password"
-              value={this.formData.password}
-              error={!!this.error}
-              helperText={this.error}
-              onChange={e => (this.formData.password = e.target.value)}
-              type="password"
-              margin="normal"
-              required
-              fullWidth
-            />
-          </CardContent>
-          <CardActions>
-            <Button color="primary" style={{ flex: 1 }} type="submit">
-              Submit
-            </Button>
-          </CardActions>
-        </form>
-      </Card>
-    );
+class SignInForm extends Form {
+  setup() {
+    return {
+      fields: [
+        {
+          name: 'email',
+          label: 'Email'
+        },
+        {
+          name: 'password',
+          label: 'Password'
+        }
+      ]
+    };
   }
 }
 
--- src/pages/sign_in_page.js
+++ src/pages/sign_in_page.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react';
-import { Grid } from 'material-ui';
+import { withRouter } from 'react-router';
+import { inject } from 'mobx-react/index';
+import autobind from 'autobind-decorator';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography
+} from 'material-ui';
 import SignInForm from '../forms/sign_in_form';
+import TextInput from '../components/forms/text_input';
+import routes from '../utils/routes';
 
+@withRouter
+@inject('store')
 class SignInPage extends Component {
+  componentWillMount() {
+    this.form = new SignInForm();
+  }
+
+  @autobind
+  async submitForm(e) {
+    try {
+      e.preventDefault();
+      await this.form.validate();
+      await this.props.store.sessionStore.signIn(this.form.values());
+      this.props.history.push(routes.tournaments());
+      this.props.store.uiStore.setAlert(
+        'success',
+        'You have successfully signed in.'
+      );
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.error_description
+      ) {
+        this.form
+          .$('password')
+          .invalidate(error.response.data.error_description);
+      } else {
+        throw error;
+      }
+    }
+  }
+
   render() {
     return (
       <Grid container justify="center">
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <SignInForm />
+          <Card>
+            <form onSubmit={this.submitForm}>
+              <CardContent>
+                <Typography type="headline">Sign in</Typography>
+                <TextInput field={this.form.$('email')} autoFocus required />
+                <TextInput
+                  field={this.form.$('password')}
+                  type="password"
+                  required
+                />
+              </CardContent>
+              <CardActions>
+                <Button color="primary" style={{ flex: 1 }} type="submit">
+                  Submit
+                </Button>
+              </CardActions>
+            </form>
+          </Card>
         </Grid>
       </Grid>
     );
