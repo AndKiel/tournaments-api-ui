7d0611f3e6dd44d4786c7c4d42b8dc09692d2d25
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2018-02-09 09:42:19 +0100

    Fix assign players button visibility

--- src/components/rounds/buttons/assign_players_button.js
+++ src/components/rounds/buttons/assign_players_button.js
@@ -18,7 +18,7 @@ class AssignPlayersButton extends Component {
   render() {
     const tournament = this.props.store.tournamentsStore.item;
 
-    if (tournament.isUserOrganiser && tournament.status !== 'ended') {
+    if (tournament.isUserOrganiser && tournament.status === 'in_progress') {
       return (
         <Tooltip title="Assign players to tables">
           <IconButton onClick={this.assignPlayers}>
