bd00edd376b4997fce4ab9d5905db71947ff24a3
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2018-02-09 09:46:12 +0100

    Update results list

--- src/components/results/results_list.js
+++ src/components/results/results_list.js
@@ -9,7 +9,6 @@ import {
   TableCell,
   Typography
 } from 'material-ui';
-import styles from './results_list.scss';
 
 @inject('store')
 @observer
@@ -22,43 +21,38 @@ class ResultsList extends Component {
     const tournament = this.props.store.tournamentsStore.item;
 
     return (
-      <div className={styles.container}>
-        <Typography type="title" paragraph>
-          Results
-        </Typography>
-        <Grid container>
-          <Grid item>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>#</TableCell>
-                  <TableCell>Competitor</TableCell>
-                  {tournament.result_names.map((name, index) => {
-                    return <TableCell key={index}>{name}</TableCell>;
-                  })}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {tournament.results.map((result, idx) => {
-                  return (
-                    <TableRow key={idx}>
-                      <TableCell numeric>{idx + 1}</TableCell>
-                      <TableCell>{result.competitor_id.name}</TableCell>
-                      {result.total.map((value, index) => {
-                        return (
-                          <TableCell key={index} numeric>
-                            {value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
+      <Grid container>
+        <Grid item>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>Competitor</TableCell>
+                {tournament.result_names.map((name, index) => {
+                  return <TableCell key={index}>{name}</TableCell>;
                 })}
-              </TableBody>
-            </Table>
-          </Grid>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tournament.results.map((result, idx) => {
+                return (
+                  <TableRow key={idx}>
+                    <TableCell numeric>{idx + 1}</TableCell>
+                    <TableCell>{result.competitor_id.name}</TableCell>
+                    {result.total.map((value, index) => {
+                      return (
+                        <TableCell key={index} numeric>
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         </Grid>
-      </div>
+      </Grid>
     );
   }
 }
