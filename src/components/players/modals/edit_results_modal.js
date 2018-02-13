1d4b4babc135148c2ef51c1aea5d07316c118dd0
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2018-02-13 10:39:13 +0100

    Refactor edit results modal

--- src/components/players/modals/edit_results_modal.js
+++ src/components/players/modals/edit_results_modal.js
@@ -1,50 +1,97 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react/index';
+import { observable } from 'mobx';
 import autobind from 'autobind-decorator';
 import {
   Button,
   Card,
   CardActions,
   CardContent,
+  FormHelperText,
   Modal,
+  TextField,
   Typography
 } from 'material-ui';
-import styles from '../../../styles/modals.scss';
-import PlayerForm from '../../../forms/player_form';
-import ArrayInput from '../../forms/array_input';
+import modalStyles from '../../../styles/modals.scss';
+import styles from './edit_results_modal.scss';
 
 @inject('store')
 @observer
 class EditResultsModal extends Component {
+  @observable resultValues = [];
+  @observable error = null;
+
   componentWillMount() {
-    this.form = new PlayerForm();
-    this.form.submitImpl = this.submitImpl;
-    this.form.set({ player: this.props.player });
-    this.field = this.form.$('player.result_values');
+    const tournament = this.props.store.tournamentsStore.item;
+    if (
+      this.props.player.result_values.length === tournament.result_names.length
+    ) {
+      this.resultValues = this.props.player.result_values.map(value => {
+        return value;
+      });
+    } else {
+      this.resultValues = tournament.result_names.map(() => {
+        return 0;
+      });
+    }
   }
 
   @autobind
-  async submitImpl() {
-    await this.props.player.update(this.form.values());
-    this.props.store.uiStore.setAlert('You have successfully updated results.');
-    this.props.onClose();
+  async onSubmit(e) {
+    e.preventDefault();
+    try {
+      this.error = null;
+      await this.props.player.update({
+        player: { result_values: this.resultValues }
+      });
+      this.props.store.uiStore.setAlert(
+        'You have successfully updated results.'
+      );
+      this.props.onClose();
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.fields) {
+        this.error = error.response.data.fields.result_values.join(', ');
+      } else {
+        throw error;
+      }
+    }
   }
 
   render() {
+    const resultNames = this.props.store.tournamentsStore.item.result_names;
+
     return (
       <Modal open={this.props.open} onClose={this.props.onClose}>
-        <Card className={styles.contents}>
-          <form onSubmit={this.form.onSubmit}>
+        <Card className={modalStyles.contents}>
+          <form onSubmit={this.onSubmit}>
             <CardContent>
               <Typography type="headline">
-                Edit results for {this.props.player.competitor_id.name}{' '}
+                Results for {this.props.player.competitor_id.name}{' '}
               </Typography>
-              <ArrayInput
-                field={this.form.$('player.result_values')}
-                allowDuplicates={true}
-                autoFocus
-              />
+              {resultNames.map((name, index) => {
+                return (
+                  <TextField
+                    key={index}
+                    id={name}
+                    name={name}
+                    label={name}
+                    value={this.resultValues[index]}
+                    onChange={e => {
+                      this.resultValues[index] = e.currentTarget.value;
+                    }}
+                    type="number"
+                    margin="normal"
+                    required
+                    className={styles.input}
+                  />
+                );
+              })}
+              {this.error && (
+                <FormHelperText className={styles.error} error>
+                  {this.error}
+                </FormHelperText>
+              )}
             </CardContent>
             <CardActions>
               <Button color="primary" style={{ flex: 1 }} type="submit">
